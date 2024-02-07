7ede88be2223f33492a7ed3f93b9e283286230c6
Liam Mah <liam.mah@ucalgary.ca>
2024-02-07 12:41:31 -0700

    Adding Flask application foundation/

---
+++ backend/microservices/DatabaseService/server.py
@@ -0,0 +1,14 @@
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+import requests
+import json
+
+app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "*"}})
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World! This is the Database Service.'
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5001)
