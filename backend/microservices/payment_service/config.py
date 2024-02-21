2107a0f8a258f7cc3c824ad963132db59d681599
Ethan <ethan.rd@outlook.com>
2024-02-20 22:37:01 -0700

    Added config files to payment and user management

---
+++ backend/microservices/payment_service/config.py
@@ -0,0 +1,3 @@
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+SQLALCHEMY_DATABASE_URI = 'postgresql://qpxuvgolkhtbmd:5c19741b0a623e5fbfbcfe99033f5c0cf6f25ced7bfd73d7bd1b8cdd49bec345@ec2-54-83-138-228.compute-1.amazonaws.com:5432/dep2418fjsc9b6'
+DEBUG = True
