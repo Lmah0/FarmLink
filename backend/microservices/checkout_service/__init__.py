443a16ed45f224f641b309a93b810fb4346fd794
Ethan <ethan.rd@outlook.com>
2024-02-21 18:33:42 -0700

    Updated order model and added temp method for deleting and recreating tables

--- backend/microservices/checkout_service/__init__.py
+++ backend/microservices/checkout_service/__init__.py
@@ -7,6 +7,7 @@ import requests
 import json
 
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import text
 
 ENVIRONMENT = 'development'
 db = SQLAlchemy()
@@ -24,12 +25,15 @@ def create_app():
 
     from . import models
 
-    @app.route('/db_create', methods=['GET'])
+    # TODO: Remove method before deploying
+    @app.route('/db_reset', methods=['GET'])
     def db_create():
+        db.session.execute(text("DROP TABLE IF EXISTS maga_order;"))
+        db.session.commit()
         with app.app_context():
             db.create_all()
 
-        return 'Tables Created!'
+        return 'Tables Reset!'
 
     @app.route('/')
     def hello_world():
--- backend/microservices/checkout_service/models.py
+++ backend/microservices/checkout_service/models.py
@@ -1,16 +1,13 @@
 from . import db
 
 class Order(db.Model):
-    __tablename__ = 'order'
+    __tablename__ = 'maga_order'
     id = db.Column(db.Integer, primary_key=True)
     purchase_date = db.Column(db.DateTime, nullable=False)
     total_cost = db.Column(db.Float, nullable=False)
     user_id = db.Column(db.Integer, nullable=False)
-    ordered_items = db.relationship('Item', secondary=order_items)
 
-
-    def __init__(self, purchaseDate, totalCost, userId, orderedItems):
+    def __init__(self, purchaseDate, totalCost, userId):
         self.purchase_date = purchaseDate
         self.total_cost = totalCost
         self.user_id = userId
-        self.ordered_items = orderedItems
