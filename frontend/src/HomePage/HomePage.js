996cf927075d52b05851c7116be167fc6a270407
ericmei123 <Eric.Mei@ucalgary.ca>
2024-03-17 00:31:23 -0600

    fixed css HomePage

--- frontend/src/HomePage/HomePage.js
+++ frontend/src/HomePage/HomePage.js
@@ -4,18 +4,19 @@ import { useNavigate } from "react-router-dom";
 
 function HomePage({ items, handleLogout, currentUserID }) {
   const navigate = useNavigate();
-  const [expandedBox, setExpandedBox] = useState(null);
+  const [expandedBoxes, setExpandedBoxes] = useState(
+    Array(items.length).fill(false)
+  );
 
-  const handleBoxClick = () => {
-    if (expandedBox === 0) {
-      setExpandedBox(null);
-    } else {
-      setExpandedBox(0);
-    }
+
+  const handleBoxClick = (index) => {
+    const newExpandedBoxes = [...expandedBoxes];
+    newExpandedBoxes[index] = !newExpandedBoxes[index];
+    setExpandedBoxes(newExpandedBoxes);
   };
 
   const goToCart = () => {
-    navigate('/Cart'); 
+    navigate("/Cart");
   };
 
   const goToProfile = () => {
@@ -27,17 +28,18 @@ function HomePage({ items, handleLogout, currentUserID }) {
   };
 
   const handleAddToCartClick = (itemId) => {
-    const addToCart = async (itemId) => { // This function will flush the cart when the user logs out
+    const addToCart = async (itemId) => {
+      // This function will flush the cart when the user logs out
       try {
         let response = await fetch("http://127.0.0.1:5002/removeFromCart", {
           method: "POST",
           body: JSON.stringify({
-            userId :  currentUserID , 
-            itemId : itemId , 
-            quantity : 1
+            userId: currentUserID,
+            itemId: itemId,
+            quantity: 1,
           }),
           headers: {
-            'Content-Type' : 'application/json',
+            "Content-Type": "application/json",
           },
         });
         if (response.ok) {
@@ -51,28 +53,22 @@ function HomePage({ items, handleLogout, currentUserID }) {
       }
     };
     addToCart(itemId);
-  }
-
-  // console.log("HomePage items:", cartItems);
+  };
 
   return (
     <>
       <div id="HomePage-Main-Container">
         <div id="HomePage-Header">
           <header>MarketPlace</header>
-          {/* <input type="text" placeholder="Search" /> */}
-
           <button style={{ marginRight: 20 }} onClick={goToCart}>
             &#x1f6d2;
           </button>
-
           <button id="profile-Button" onClick={goToProfile}>
             <img
               src="https://media.istockphoto.com/id/1131164548/vector/avatar-5.jpg?s=612x612&w=0&k=20&c=CK49ShLJwDxE4kiroCR42kimTuuhvuo2FH5y_6aSgEo="
               alt="Person Emoji"
             />
           </button>
-
           <button id="logout-Button" onClick={handleLogoutClick}>
             Logout
           </button>
@@ -80,26 +76,25 @@ function HomePage({ items, handleLogout, currentUserID }) {
 
         <div
           id="main-ItemBox-container"
-          className={expandedBox === 0 ? "expanded-main-container" : ""}
+          className={expandedBoxes.includes(true) ? "expanded-main-container" : ""}
         >
           {items.map((item, index) => (
             <div
               id="Item-box"
-              className={expandedBox === 0 ? "expanded-item-box" : ""}
+              className={expandedBoxes[index] ? "expanded-item-box" : ""}
               key={item.id}
             >
-              <div id={expandedBox === 0 ? "button-img-wrapper" : ""}>
+              <div id="button-img-wrapper">
                 <img
                   id="Item-box-img"
                   src="https://cdn.britannica.com/22/215522-050-8315BB78/green-grass-close-up.jpg"
                   alt="Product item"
-                  onClick={handleBoxClick}
+                  onClick={() => handleBoxClick(index)}
                 />
-
                 <button
                   id="add-item-to-cart"
-                  className={expandedBox === 0 ? "" : "hidden-element"}
-                  onClick={ handleAddToCartClick.bind(this, item.id)}
+                  className={expandedBoxes[index] ? "" : "hidden-element"}
+                  onClick={() => handleAddToCartClick(item.id)}
                 >
                   <img
                     src="https://static-00.iconduck.com/assets.00/sign-plus-icon-2048x2047-jdkmk1r1.png"
@@ -107,24 +102,18 @@ function HomePage({ items, handleLogout, currentUserID }) {
                   />
                 </button>
               </div>
-
-              <h4 className={expandedBox === 0 ? "" : "hidden-element"}>
+              <h4 className={expandedBoxes[index] ? "" : "hidden-element"}>
                 Seller: {item.posting_author}
               </h4>
-
               <h3>{item.posting_item["name"]}</h3>
-
               <h4>Price: ${item.posting_item["price"]}</h4>
-
-              <h4 className={expandedBox === 0 ? "" : "hidden-element"}>
-                Quanity Avaliable: {item.quantity}
+              <h4 className={expandedBoxes[index] ? "" : "hidden-element"}>
+                Quantity Available: {item.quantity}
               </h4>
-
-              <h4 className={expandedBox === 0 ? "hidden-element" : ""}>
+              <h4 className={expandedBoxes[index] ? "hidden-element" : ""}>
                 Seller: {item.posting_author}
               </h4>
-
-              <p className={expandedBox === 0 ? "" : "hidden-element"}>
+              <p className={expandedBoxes[index] ? "" : "hidden-element"}>
                 Description: {item.description}
               </p>
             </div>
