345b4f1498003bfbeb1c2d12b9e5b2777ace8d49
BrianUCalgary <114112708+BrianUCalgary@users.noreply.github.com>
2024-03-17 10:24:27 -0600

    Update to Payment
    Changed API correctly to createOrder
    Added a check so that fields should be filled, card number should be 16 digits, and security number should be 3 digits

--- frontend/src/Payment/Payment.js
+++ frontend/src/Payment/Payment.js
@@ -48,16 +48,31 @@ function Payment() {
     Object.entries(values).forEach(([key, value]) => {
       console.log(`${key}: ${value}`);
     });
+
+    if (values.textbox1.length !== 16) {
+      console.log("Card number must be 16 digits");
+      return;
+    }
+    if (values.textbox2.length !== 3) {
+      console.log("Security number must be 3 digits");
+      return;
+    }
+    const requiredFields = ['textbox1', 'textbox2', 'textbox3', 'textbox4', 'textbox5', 'textbox6', 'textbox8', 'textbox9', 'textbox10'];
+    for (const field of requiredFields) {
+      if (!values[field]) {
+        console.log(`${field} is required`);
+        return;
+      }
+    }
     console.log(totalPrice)
     try {
-      let response = await fetch("http://127.0.0.1:5000/checkStock", {
+      let response = await fetch("http://127.0.0.1:5000/createOrder", {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
         },
         body: JSON.stringify(userID),
       });
-
       if (response.ok) {
         let jsonRes = await response.json();
         console.log(jsonRes, "JSON RES");
