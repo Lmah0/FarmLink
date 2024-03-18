8f7e3afdfc7dc1efd686d6177826878a876beb9e
Theodore Hoang <theodore.hoang@ucalgary.ca>
2024-03-18 00:01:13 -0600

    add error checking for user management

--- backend/microservices/user_management_service/UserManagementService.py
+++ backend/microservices/user_management_service/UserManagementService.py
@@ -22,10 +22,18 @@ class UserManagementService(IUserManagementService.IUserManagementService):
         farmerPid = data['farmer_pid']
         profileBio = data['profile_bio']
 
+        if not name or not phoneNumber or not emailAddress or not password or role == None:
+            return jsonify({'message': 'name, phone number, email address, password and role are required.'}), 400
         try: # check that role is in ROLE enum
             role = models.Role[role]
         except KeyError:
-            return jsonify({'message': 'Invalid role.'})
+            return jsonify({'message': 'Invalid role.'}), 400
+        
+        if role == models.Role.FARMER and not farmerPid:
+            return jsonify({'message': 'Farmer PID is required for farmers.'}), 400
+        elif role == models.Role.NONFARMER and farmerPid:
+            return jsonify({'message': 'Farmer PID is not required for non-farmers.'}), 400
+        
         
 
         newUser = models.User(name, phoneNumber, emailAddress, password, role, profileBio, farmerPid)
@@ -39,6 +47,9 @@ class UserManagementService(IUserManagementService.IUserManagementService):
         emailAddress = data['email_address']
         password = data['password']
         user = models.User.query.filter_by(email_address=emailAddress, password=password).first()
+
+        if not emailAddress or not password:
+            return jsonify({'message': 'Email address and password are required.'}), 400
 
         if user is None:
             return jsonify({'message': 'Invalid email address or password.'}), 401
