3d42fb04ca0f10cdea8684e15ca59e5f4c7221b2
Liam Mah <liam.mah@ucalgary.ca>
2024-03-19 15:06:37 -0600

    Adding finalization for login/signup page.

--- frontend/src/HomePage/HomePageEmpty.js
+++ frontend/src/HomePage/HomePageEmpty.js
@@ -1,5 +1,9 @@
 import "./HomePageEmpty.css";
 import { useNavigate } from "react-router-dom";
+import { Typed } from "react-typed";
+import React from "react";
+import logo from "./../Images/agriculture.png";
+
 
 function HomePageEmpty() {
   const navigate = useNavigate();
@@ -12,14 +16,29 @@ function HomePageEmpty() {
     navigate("/login");
   };
 
+  const el = React.useRef(null);
+  
+  React.useEffect(() => {
+      const typed = new Typed(el.current, {
+          strings: ["Branching the agricultural market starts here."],
+          typeSpeed: 50,
+      });
+      
+      return () => {
+          // Destroy Typed instance during cleanup to stop animation
+          typed.destroy();
+      };
+
+  }, []);
+
   return (
     <div>
       <div id="HomePage-Main-Container">
         <div className="login-page">
           <div className="motto typewriter">
-            <h1>Branching the agricultural market starts here.</h1>
+            <h1><span ref={el} /></h1>
           </div>
-          <div>
+          <div className="auth-buttons">
             <button className="auth-button" onClick={handleSignUpClick}>
               Sign Up
             </button>
