9cae355c56be41e5baa77fd112a00eb5fe1cfcd8
Liam Mah <liam.mah@ucalgary.ca>
2024-03-19 20:41:32 -0600

    Changing login page to be consistent with the signup page.

--- frontend/src/UserPages/LoginPage.js
+++ frontend/src/UserPages/LoginPage.js
@@ -69,11 +69,11 @@ const LoginPage = ({handleSetProfile}) => {
         <h2>Login</h2>
         <div className="form-group email-input"> 
           <label>Email</label>
-          <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+          <input className="login-field" type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
         </div>
         <div className="form-group">
           <label>Password</label>
-          <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+          <input className="login-field" type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
         </div>
         <button id="submit-login" type="submit">Login</button>
       </form>
